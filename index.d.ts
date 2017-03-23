5626e8f416e94d4160238fc4a5ed872ee30764cb
Felix Rieseberg <felix@felixrieseberg.com>
2017-03-23 12:50:05 -0700

    :memo: Better types

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,6 @@
-type SessionState = 'SESSION_SCREEN_IS_LOCKED' | 'SESSION_ON_CONSOLE_KEY' | 'UNKNOWN';
-type DoNotDisturbState = 'DO_NOT_DISTURB';
-
 declare module 'macos-notification-state' {
-  export function getNotificationState(): 'UNKNOWN_ERROR' | SessionState | DoNotDisturbState;
+  export function getNotificationState(): 'UNKNOWN_ERROR' | SessionState | 'DO_NOT_DISTURB';
   export function getSessionState():  SessionState;
   export function getDoNotDisturb(): boolean;
+  export type SessionState = 'SESSION_SCREEN_IS_LOCKED' | 'SESSION_ON_CONSOLE_KEY' | 'UNKNOWN';
 }
