6ed3a4f90e98335bf28f8450ddbd19cef1760ae8
nbashkankov <nbashkankov@outlook.com>
2020-10-12 12:02:42 +0300

    Update do-not-disturb.mm
    modified version check

--- lib/do-not-disturb.mm
+++ lib/do-not-disturb.mm
@@ -1,10 +1,9 @@
 #import <Foundation/Foundation.h>
-#import <AppKit/NSApplication.h>
 
 bool getDoNotDisturb() {
   bool doNotDisturb;
 
-  if (floor(NSAppKitVersionNumber) < 11) {
+  if ([[NSProcessInfo processInfo] operatingSystemVersion].majorVersion < 11) {
     doNotDisturb = [[[[NSUserDefaults alloc] initWithSuiteName:@"com.apple.notificationcenterui"] objectForKey:@"doNotDisturb"] boolValue];
   } else {
     doNotDisturb = [[[[NSUserDefaults alloc] initWithSuiteName:@"com.apple.controlcenter"] objectForKey:@"NSStatusItem Visible DoNotDisturb"] boolValue];
