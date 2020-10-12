38d4abee980467622714fcc880fd72bc02e33008
nbashkankov <nbashkankov@outlook.com>
2020-10-12 12:45:16 +0300

    Update do-not-disturb.mm
    detect version logic

--- lib/do-not-disturb.mm
+++ lib/do-not-disturb.mm
@@ -2,8 +2,9 @@
 
 bool getDoNotDisturb() {
   bool doNotDisturb;
-
-  if ([[NSProcessInfo processInfo] operatingSystemVersion].majorVersion < 11) {
+  NSOperatingSystemVersion version = [[NSProcessInfo processInfo] operatingSystemVersion];
+  bool isBigSur = version.majorVersion == 11 || (version.majorVersion == 10 && version.minorVersion > 15);
+  if (!isBigSur) {
     doNotDisturb = [[[[NSUserDefaults alloc] initWithSuiteName:@"com.apple.notificationcenterui"] objectForKey:@"doNotDisturb"] boolValue];
   } else {
     doNotDisturb = [[[[NSUserDefaults alloc] initWithSuiteName:@"com.apple.controlcenter"] objectForKey:@"NSStatusItem Visible DoNotDisturb"] boolValue];
